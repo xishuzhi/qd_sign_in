588fa48953986936a5e4a1359d4f16bc6b1735ff
xixixi <xixi@xi.com>
2017-03-22 12:15:11 +0800

    origin 1-3

--- qd_xian_shi_mian_fei.py
+++ qd_xian_shi_mian_fei.py
@@ -10,6 +10,10 @@ sys_code = sys.getfilesystemencoding()
 
 url = "https://f.qidian.com/"
 
+SAVE_TYPE_TEXT_NOWARP = 0
+SAVE_TYPE_TEXT_WARP = 1
+SAVE_TYPE_TEXT_NOWARP_XHTML = 2
+SAVE_TYPE_TEXT_WARP_XHTML = 3
 
 #从免费书列表中获取限免书籍信息
 def get_limit_list():
@@ -52,8 +56,8 @@ def get_html_by_browser(url):
     browser.quit()
     return html_source
 
-#保存章节和内容到文件
-def save_volume(url,filePath):
+#保存章节和内容到文件,ytpe 为保存方式
+def save_volume(url,filePath,type=SAVE_TYPE_TEXT_NOWARP):
     ht = get_html(url)
     # ht = get_html("http://vipreader.qidian.com/chapter/3155120/54155582")
     # print(ht)
@@ -63,17 +67,22 @@ def save_volume(url,filePath):
         book_data = metaSoup.find('div', attrs={'class': 'read-content j_readContent'})
         #print(book_info.get_text())
         #print(book_data.get_text())
-
         with open(filePath, 'wb') as f:
             if f:
                 f.write(book_info.get_text().encode('utf-8'))
-                f.write(book_data.get_text().encode('utf-8'))
+                if type == SAVE_TYPE_TEXT_NOWARP:
+                    f.write(book_data.get_text().encode('utf-8'))
+                if type == SAVE_TYPE_TEXT_WARP:
+                    text = book_data.get_text()
+                    text = text.replace('　　', '\n　　')
+                    f.write(text.encode('utf-8'))
                 f.close()
-        # with open(filePath+'.xhtml', 'wb') as fx:
-        #     if fx:
-        #         fx.write(book_info.get_text().encode('utf-8'))
-        #         fx.write(book_data.encode('utf-8'))
-        #         fx.close()
+        if type == SAVE_TYPE_TEXT_NOWARP_XHTML or type == SAVE_TYPE_TEXT_WARP_XHTML:
+            with open(filePath+'.xhtml', 'wb') as fx:
+                if fx:
+                    fx.write(book_info.get_text().encode('utf-8'))
+                    fx.write(book_data.encode('utf-8'))
+                    fx.close()
     except OSError as err:
         print("OSError:"+err)
     except IOError as err:
@@ -136,6 +145,24 @@ if __name__ == "__main__":
     #获取当前路径
     print(os.getcwd())
     thisPath = os.getcwd()
+    download_type = SAVE_TYPE_TEXT_NOWARP
+    config_file_path = thisPath+'\\'+'qd_download_type.config'
+    if(os.path.exists(config_file_path)):
+        with open(config_file_path) as f:
+            data = f.read();
+            try:
+                data = data[data.find('download_type'):]
+            except:
+                print('config file error !!')
+            c = data.split('=')[1].strip().lstrip()
+            if c == '0':
+                download_type = SAVE_TYPE_TEXT_NOWARP
+            elif c == '1':
+                download_type = SAVE_TYPE_TEXT_WARP
+            elif c == '2':
+                download_type = SAVE_TYPE_TEXT_NOWARP_XHTML
+            elif c == '3':
+                download_type = SAVE_TYPE_TEXT_WARP_XHTML
 
     basename = os.path.splitext(os.path.basename(__file__))[0]
 
@@ -164,7 +191,7 @@ if __name__ == "__main__":
                     count_volume += 1
                     continue
             else:
-                volume_name = save_volume(v_url,path).strip().lstrip()
+                volume_name = save_volume(v_url,path,download_type).strip().lstrip()
                 print('  downlaod:<%s>:%s : %s' % (book_name.replace('\n', ''),volume_name.decode('utf-8'),v_url))
             count_volume += 1
         count_book += 1
