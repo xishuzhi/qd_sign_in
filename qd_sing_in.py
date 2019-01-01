4fcd52313916e1e16d5f3244c0458ec77fa1cab0
xixixi <xixi@xi.com>
2019-01-01 11:21:56 +0800

    更新起点签到类，测试中3……

--- qd_sing_in.py
+++ qd_sing_in.py
@@ -66,6 +66,9 @@ class qd_sing_in:
     def __init__(self, br_name=''):
         self.url = "https://my.qidian.com/level"
         self.url_qd = "https://www.qidian.com"
+        self.url_login='https://passport.qidian.com/?appid=10&areaid=1&auto=1&autotime=30&version=1.0&ticket=1&target=top&popup=0&source=pc&returnurl=https%3A%2F%2Fmy.qidian.com'
+        self.url_passport = 'https://passport.qidian.com'
+        self.position_url_passport = 27
         self.cookies_file = 'qd.cookies.txt'
         self.cookies = ''
         self.step = {0: 300, 1: 300, 2: 600, 3: 1200, 4: 1800, 5: 3600, 6: 3600, 7: 3600, 8: 3600}
@@ -96,10 +99,10 @@ class qd_sing_in:
                 f.close()
         return result
 
-    def open_qd_level(self):
+    def open_qd_level(self, refresh=False):
         result = False
         try:
-            if self.url != self.browser.current_url:
+            if self.url != self.browser.current_url or refresh:
                 self.browser.get(self.url)
             result = WebDriverWait(self.browser, 120).until(must_get_url(self.url))
         except WebDriverException as e:
@@ -148,12 +151,21 @@ class qd_sing_in:
             else:
                 print('时间未到：'+btn.text)
                 t = btn.text.split(':')
-                second = int(t[0])*60 + int(t[1])
-                print('距离下次签到剩余时间：'+str(second)+'秒')
-                return False, second
+                if len(t) == 2:
+                    second = int(t[0])*60 + int(t[1])
+                    print('距离下次签到剩余时间：'+str(second)+'秒')
+                    return False, second
+                else:
+                    return True, -1
         except WebDriverException:
             return True, -1
             # pass
+
+    def check_login_failed(self):
+        if self.browser.current_url == self.url_login or self.browser.current_url[0:self.position_url_passport] == self.url_passport:
+            return True
+        else:
+            return False
 
     # 执行签到函数
     def sing_in(self):
@@ -171,7 +183,7 @@ class qd_sing_in:
                     if n == '已领取':
                         count += 1
             self.index = count
-            print('总签到数量为：' + str(count))
+            print('总领取经验数量为：' + str(count))
             if self.index == 8:
                 print('今天的签到已经完成')
                 sing_finish = True
@@ -186,7 +198,7 @@ class qd_sing_in:
                 print('时间未到：'+btn.text)
                 t = btn.text.split(':')
                 second = int(t[0])*60 + int(t[1])
-                print('距离下次签到剩余时间：'+str(second)+'秒')
+                print('距离下次领取经验剩余时间：'+str(second)+'秒')
 
         except WebDriverException:
             sing_finish = True
@@ -201,24 +213,30 @@ def main():
     qd = qd_sing_in()
     # 登录起点
     if qd.login_qd():
-        # 签到结果是否完成,签到序号,距离下一次签到剩余等待时间（秒）
-        # is_finish, index, seconds = qd.sing_in()
+        # 签到结果是否完成,距离下一次签到剩余等待时间（秒）
         is_finish, seconds = qd.sing_in()
+        try_count = 0
         while True:
             os.system('cls')
             dt.update()             # 更新时间记录
             if dt.check_new_day():
-                print('第二天了，开始签到')
-                # 执行签到函数
-                is_finish, seconds = qd.sing_in()
-                # 新的一天，刷新日期
+                print('第二天了，开始领取经验')
+                # 刷新页面
+                if qd.open_qd_level(True):
+                    # 执行领取经验函数
+                    is_finish, seconds = qd.sing_in()
+                # 新的一天，刷新日期，不管是否成功领取经验都刷新日期
                 dt.refresh_day()
             if not is_finish:
                 is_finish, seconds = qd.check_next_time()
                 if dt.check_time_seconds(seconds):
                     is_finish, seconds = qd.sing_in()
             else:
-                print('今天签到已经完成')
+                print('今天领取经验已经完成')
+            if qd.check_login_failed():
+                try_count += 1
+            if try_count > 5:
+                break
             time.sleep(10)
     qd.quit()
 
@@ -234,30 +252,38 @@ def get_time():
 
 
 def test():
+    # 初始化计时工具
     dt = NextDayTools()
+    # 初始化起点签到类
     qd = qd_sing_in()
-    if qd.open_qd():
+    # 登录起点
+    if qd.login_qd():
         # 签到结果是否完成,签到序号,距离下一次签到剩余等待时间（秒）
-        is_finish, index, seconds = qd.sing_in()
+        is_finish, seconds = qd.sing_in()
+        try_count = 0
         while True:
             os.system('cls')
-            dt.update()             # 更新时间记录
+            dt.update()  # 更新时间记录
             if dt.check_new_day():
                 print('第二天了，开始签到')
-                # 执行签到函数
-                is_finish, index, seconds = qd.sing_in()
-                # 新的一天，刷新日期
+                # 刷新页面
+                if qd.open_qd_level(True):
+                    # 执行签到函数
+                    is_finish, seconds = qd.sing_in()
+                # 新的一天，刷新日期，不管是否成功签到都刷新日期
                 dt.refresh_day()
             if not is_finish:
+                is_finish, seconds = qd.check_next_time()
                 if dt.check_time_seconds(seconds):
-                    is_finish, index, seconds = qd.sing_in()
+                    is_finish, seconds = qd.sing_in()
             else:
                 print('今天签到已经完成')
-            # print('签到结果:%s,序号:%s,等待时间:%s' % (is_finish, index, seconds))
-            print('签到序号:'+index)
+            if qd.check_login_failed():
+                try_count += 1
+            if try_count > 5:
+                break
             time.sleep(10)
     qd.quit()
-
 
 
 if __name__ == "__main__":
@@ -266,53 +292,3 @@ if __name__ == "__main__":
 
 
 
-
-
-"""
-//*[@id="online-exp-get5"]
-#online-exp-get5
-
-body > div.fix-layout.column-wild-content > div.fl-right-wrap > div.exp-tasks > div.month-ticket.award-for-login > ul > li:nth-child(1) > a
-
-/html/body/div[8]/div[2]/div[2]/div[2]/ul/li[1]/a
-//*[@id="online-exp-get5"]
-//*[@id="online-exp-get10"]
-/html/body/div[8]/div[2]/div[2]/div[2]/ul/li[3]/a
-/html/body/div[8]/div[2]/div[2]/div[2]/ul/li[4]/a
-/html/body/div[8]/div[2]/div[2]/div[2]/ul/li[3]/a
-
-弹出按钮
-//*[@id="btnClose_dialog_869"]/span
-//*[@id="btnClose_dialog_565"]
-
-弹出框
-//*[@id="alert_box_container_dialog_869"]/table/tbody/tr[2]/td[2]/div/div[1]/strong
-//*[@id="alert_box_container_dialog_869"]/table/tbody/tr[2]/td[1]
-x
-//*[@id="alert_box_container_dialog_869"]/table/tbody/tr[2]/td[2]/div/div[3]/text()
-//*[@id="anchorclose_dialog_869"]
-
-iedriver = r"F:\temp\IEDriverServer.exe"
-os.environ["webdriver.ie.driver"] = iedriver
-browser = webdriver.Ie(iedriver)
-http://my.qidian.com/level
-
-
-#elTaskWrap > li:nth-child(1) > a
-
-//*[@id="elTaskWrap"]/li[1]/a
-
-//*[@id="elTaskWrap"]/li[2]/span
-
-//*[@id="elTaskWrap"]/li[1]/a
-
-//*[@id="elTaskWrap"]/li[3]/span
-
-//*[@id="elTaskWrap"]/li[2]/span
-
-<a href="javascript:;" class="ui-button ui-button-small elGetExp " data-num="1" data-timeleft="0" data-vip="2">领取</a>
-
-#elOldexp
-
-//*[@id="elOldexp"]
-"""
